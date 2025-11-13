111d944a352df4a9f88daec52d3e02417adc53a6
Ganesh <ganeshkaradagi@gmail.com>
2025-11-13 14:49:59 +0530

    demo added

--- demo.py
+++ demo.py
@@ -1,10 +1,4 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
+import os
+mongo_db_url = os.getenv("MONGODB_URL")
 
-
-logging.info("Demo file for MongoDB operations")
-
-try:
-    a = 1 / 0
-except Exception as e:
-    raise USvisaException(e, sys)
+print(mongo_db_url)
